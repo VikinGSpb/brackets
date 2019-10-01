5ee92f5fabb0c7c8ff692808dc99ce5858cc38ff
VikinGSpb <imbot@inbox.ru>
2019-10-01 23:35:38 +0300

    feat: add first version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
     let OpenArr = [];
     let CloseArr = [];
+    let strArr = str.split('');
     for(let i = 0; i < bracketsConfig.length; i++)
     {
         OpenArr[i] = bracketsConfig[i][0];
         CloseArr[i] = bracketsConfig[i][1]; 
     }
+    if( strArr.length % 2 != 0 ) return false;
+    let numb = null;
+    for(let i = 0; i < strArr.length; i++)
+    {
+        for(let j = 0; j < OpenArr.length; j++)
+        {
+            if(str[i] == OpenArr[j])
+            {
+                numb = i;
+                break;
+            }
+        }
+        for(let k = i + 1; k < strArr.length; k++)
+        {
+            if(strArr[k] == CloseArr[numb])
+            {
+                delete strArr[i];
+                delete strArr[k];
+                break;
+            }
+        }
+    }
+    for(let i = 0; i < strArr.length; i++)
+    {
+        if(strArr[i]) return false;
+    }
+    return true;
 }
