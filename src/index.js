ea3d20fd6c22ed129f8515d0c2610ba26ce9054f
VikinGSpb <imbot@inbox.ru>
2019-10-01 23:09:14 +0300

    feat: add arrays for open and close brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let OpenArr = [];
+    let CloseArr = [];
+    for(let i = 0; i < bracketsConfig.length; i++)
+    {
+        OpenArr[i] = bracketsConfig[i][0];
+        CloseArr[i] = bracketsConfig[i][1]; 
+    }
 }
